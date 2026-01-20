8a999876757eec00d636674a3d45723b44e4cdb4
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-20 17:50:16 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoiceController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome'); 
+})->name('home');
+
+Route::get('/invoice', [InvoiceController::class, 'create'])->name('invoice.form');
+Route::post('/invoice', [InvoiceController::class, 'store'])->name('invoice.submit');
