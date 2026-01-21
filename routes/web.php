6f37dad0397d7c79d233afd1d9d2239af4f1015a
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-21 18:18:28 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\InvoiceController;
 Route::get('/', [InvoiceController::class, 'home'])->name('home');
 Route::get('/invoice-create', [InvoiceController::class, 'create'])->name('invoice.form');
 Route::post('/invoice-store', [InvoiceController::class, 'store'])->name('invoice.submit');
+Route::get('/invoice-list', [InvoiceController::class, 'list'])->name('invoice.list');
 Route::get('/invoice-pdf/{id}', [InvoiceController::class, 'pdf'])->name('invoice.pdf');
