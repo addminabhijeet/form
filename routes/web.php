9fae33e328f7e2bc0cb69bd6c22c89dbcf7cd879
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-21 17:45:06 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,4 @@ Route::get('/', function () {
 
 Route::get('/invoice-create', [InvoiceController::class, 'create'])->name('invoice.form');
 Route::post('/invoice-store', [InvoiceController::class, 'store'])->name('invoice.submit');
-Route::post('/invoice-pdf', [InvoiceController::class, 'pdf'])->name('invoice.pdf');
+Route::get('/invoice-pdf', [InvoiceController::class, 'pdf'])->name('invoice.pdf');
