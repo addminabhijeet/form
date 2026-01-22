09f3730cff685d570f589447d2902f1ec708556c
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-22 19:36:23 +0530

    m

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -50,12 +50,10 @@
                         </div>
                     @endif
 
-                    @if (session('error'))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            {{ session('error') }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                        </div>
-                    @endif
+                    @error('field_name')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+
 
 
                     <form method="POST"
